027c6346013f4259d190028e3da6dc847530fa47
943hfz8324hf89z342hf7834 <108883510+943hfz8324hf89z342hf7834@users.noreply.github.com>
2022-09-15 17:37:46 +0200

    Create test.js

---
+++ test.js
@@ -0,0 +1,10 @@
+// ==UserScript==
+// @name         testscript
+// @namespace    testscript
+// @version      1
+// @match        https://google.com/*
+// @run-at       document-start
+// @grant        GM_xmlhttpRequest
+// ==/UserScript==
+
+alert('working');
