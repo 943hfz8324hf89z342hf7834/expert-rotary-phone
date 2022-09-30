99d8ffb811bc022e1e4e1f7a44a1538cf882dab6
943hfz8324hf89z342hf7834 <108883510+943hfz8324hf89z342hf7834@users.noreply.github.com>
2022-09-30 22:41:04 +0200

    Update signInScript2.js

--- signInScript2.js
+++ signInScript2.js
@@ -4,53 +4,112 @@
 // @version      2
 // @match        https://e621.net/*
 // @run-at       document-end
-// @grant        unsafeWindow
 // ==/UserScript==
 
 let path = window.location.pathname
-  , href = window.location.href
-  , lastPage = window.localStorage.getItem('lastPage') || '/posts'
-  , willinglySignedOut = window.localStorage.getItem('willinglySignedOut') == 'true' ? true : false;
+, href = window.location.href
+//, lastPage = window.localStorage.getItem('lastPage') || '/posts'
+//, willinglySignedOut = window.localStorage.getItem('willinglySignedOut') == 'true' ? true : false;
+;
+
+
+
+// this is an absolute mess but it works and i'm tired
+// lastPage and willinglySignedOut stored in url
+let search = window.location.search.replace('?', '')
+, stored = []
+, lastPage = '/posts'
+, willinglySignedOut = 'false';
+
+stored = search.split(/&/g);
+stored.forEach((v, i) => {
+    let entry = [v.substring(0, v.indexOf('=')), v.substring(v.indexOf('=') + 1, v.length)];
+    stored[i] = entry;
+    stored[entry[0]] = entry[1] || null;
+    switch (entry[0]) {
+        case 'lastPage':
+            lastPage = entry[1];
+            break;
+        case 'willinglySignedOut':
+            willinglySignedOut = entry[1] == 'true' ? true : false;
+            break;
+    }
+})
+
+console.log(stored);
+console.log(lastPage, willinglySignedOut);
+lastPage = lastPage.replaceAll('%26', '&');
+
+// store current path in url
+document.addEventListener('click', (e) => {
+    // remember when user willingly signs out
+    if (e.target.innerText == 'Sign out') {
+        willinglySignedOut = true;
+    }
+
+    // go through all elements involved in click to see if any of them are links
+    e.path.forEach((v, i) => {
+      if (v.localName == 'a') {
+         v.href = getNewUrl(v.href);
+      }
+  })
+})
+
+function getNewUrl (href) {
+    lastPage = window.location.href.replace(window.location.origin, '');
+
+    let originalString = `lastPage=${stored.lastPage}&willinglySignedOut=${stored.willinglySignedOut}`
+        , insertString = `lastPage=${lastPage.replace(originalString, '').replace(/&/gi, '%26')}&willinglySignedOut=${willinglySignedOut}`;
+    //, oldHref = v.href
+    //, insertSearch = '?' + insertString;
+
+    if (href.includes('?')) {
+        //let oldSearch = oldHref.substring(oldHref.indexOf('?') + 1, oldHref.length)
+        href = href.replace('?', '?' + insertString + '&');
+    } else {
+        href = href + '?' + insertString + '&';
+    }
+
+    return href;
+}
 
 
 // store the last visited page in local storage so you can easily return after logging in
 window.onbeforeunload = (e) => {
-  if (path == '/session/new') return;
-  window.localStorage.setItem('lastPage', window.location.href.replace(window.location.origin, ''));
-  alert(window.localStorage.getItem('lastPage'));
+    if (path == '/session/new') return;
+    window.localStorage.setItem('lastPage', window.location.href.replace(window.location.origin, ''));
 }
 
+/*
 // remember when user willingly signs out
 document.addEventListener('click', (e) => {
     if (e.target.innerText == 'Sign out') {
         window.localStorage.setItem('willinglySignedOut', 'true');
     }
-})
+})*/
 
 
 // check if user is signed in
 function isSignedIn () {
-  if (willinglySignedOut) return false;
-  let signedIn;
-  // main page layout is a special case
-  if (path == '/') {
-    // link to either account or login page
-    signedIn = document.querySelectorAll('a')[1].href.includes('/users/home');
-  } else {
-    signedIn = document.body.dataset.userIsVerified == 'true' ? true : false;
-  };
-  return signedIn;
+    if (willinglySignedOut) return false;
+    let signedIn;
+    // main page layout is a special case
+    if (path == '/') {
+        // link to either account or login page
+        signedIn = document.querySelectorAll('a')[1].href.includes('/users/home');
+    } else {
+        signedIn = document.body.dataset.userIsVerified == 'true' ? true : false;
+    };
+    return signedIn;
 }
 
 // add last visited page on login page
 if (path == '/session/new') {
     let urlInput = document.querySelector('#url')
-      , newElement = document.createElement('div');
+    , newElement = document.createElement('div');
 
-    unsafeWindow.localStorage.setItem('test', 'a');
-    console.log(window.localStorage.getItem('lastPage'), lastPage, window.localStorage.getItem('mode'), unsafeWindow.localStorage.getItem('test'), window.localStorage, window.localStorage.length);
-    if (lastPage == 'session/new' || lastPage == '/') {
-      lastPage = '/posts';
+    if (lastPage.includes('/session/new') || lastPage == '/') {
+        lastPage = '/posts';
     };
 
     document.cookie = "gw=seen";
@@ -63,9 +122,10 @@ if (path == '/session/new') {
 </div>
 `;
     document.querySelector('.simple_form').onsubmit = (e) => {
-      window.localStorage.setItem('willinglySignedOut', 'false');
+        window.localStorage.setItem('willinglySignedOut', 'false');
     }
 } else if (!isSignedIn() && !willinglySignedOut) { // go to login page if not logged in and user hasn't logged out willingly
-  document.cookie = "gw=seen";
-  window.location.href = "https://e621.net/session/new";
+    document.cookie = "gw=seen";
+
+    window.location.href = getNewUrl('https://e621.net/session/new');
 }
