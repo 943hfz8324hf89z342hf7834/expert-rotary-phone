b66b2705f799cb9ef72cc9576012062ae8466792
943hfz8324hf89z342hf7834 <108883510+943hfz8324hf89z342hf7834@users.noreply.github.com>
2023-03-02 21:07:49 +0100

    Update betterEruda.js

--- betterEruda.js
+++ betterEruda.js
@@ -2,7 +2,7 @@
 // @name         Use version of eruda on github instead of the one available in the app store
 // @version      1
 // @match        *
-// @run-at       document-end
+// @run-at       document-start
 // ==/UserScript==
 
 // safeWindow jailbreak
@@ -11,83 +11,56 @@ document.body.appendChild(frame);
 const realWindow = frame.contentWindow.parent;
 frame.remove();
 
+((window, safeWindow) => {
+  window.safeWindow = safeWindow
 
-let replacedEruda = false;
-let toggleCount = 0;
+  let replacedEruda = false;
 
-for (const key in realWindow) {
-  try {
-    console.log([key, realWindow[key]])
-  } catch (e) {
-    console.error([key, e])
+  window.reqListener = function reqListener(e) {
+    replacedEruda = true;
+    window.removeEventListener("message", messageListenerFunct);
+  
+    window.oldEruda = safeWindow.eruda;
+
+    oldEruda.hide()
+    Function(e.target.responseText)();
+    eruda.init();
+    eruda.show();
+
+    eruda.add(erudaDom);
+
+    let navItems = eruda._$el.find('.eruda-nav-bar-item');
+    navItems.first().before(
+      navItems[navItems.length - 2]
+    )
+
+    //realWindow.eruda._entryBtn.hide()
   }
-}
 
-window.reqListener = function reqListener(e) {
-  replacedEruda = true;
-  /*if (eruda?._devTools._isShow) {
-    eruda.hide();
-  }*/
-  
-  realWindow.oldEruda = eruda;
-  
-  for (const key in realWindow) {
-    try {
-      console.log([key, realWindow[key]])
-    } catch (e) {
-      console.error([key, e])
+  window.replaceEruda = function replaceEruda () {
+    const req = new XMLHttpRequest();
+    req.addEventListener("load", window.reqListener);
+    req.open("GET", "https://cdn.jsdelivr.net/npm/eruda");
+    req.send();
+  }
+
+  window.messageListenerFunct = function messageListener (event) {
+    console.log(event);
+    if (event.data &&
+      event.data.direction === "from-content-script" &&
+      event.data.message === "toggle") {
+        console.log('t+' + performance.now() + ' ms: eruda toggled');
+    
+        if (!eruda) {
+          console.error("couldn't find eruda");
+          return;
+        }
+        if (replacedEruda) return;
+
+        replaceEruda();
     }
   }
 
-  setTimeout(() => {
-  eruda.hide()
-  Function(e.target.responseText)();
-  realWindow.eruda.init();
-  realWindow.eruda.show();
+  window.addEventListener("message", messageListenerFunct);
 
-  realWindow.eruda.add(window.erudaDom);
-
-  let navItems = realWindow.eruda._$el.find('.eruda-nav-bar-item');
-  navItems.first().before(
-    navItems[navItems.length - 2]
-  )
-
-  //realWindow.eruda._entryBtn.hide()
-  }, 5000)
-}
-
-window.replaceEruda = function replaceEruda () {
-  const req = new XMLHttpRequest();
-  req.addEventListener("load", window.reqListener);
-  req.open("GET", "https://cdn.jsdelivr.net/npm/eruda");
-  req.send();
-}
-
-window.messageListenerFunct = function messageListener (event) {
-  console.log(event);
-  if (event.data &&
-    event.data.direction === "from-content-script" &&
-    event.data.message === "toggle") {
-      ++toggleCount;
-      console.log('t+' + performance.now() + ' ms: eruda toggled');
-    
-      //if (toggleCount < 2) return; // quick fix for testing
-      if (!eruda) {
-        console.error("couldn't find eruda");
-        for (const key in window) {
-          try {
-            console.log([key, window[key]])
-          } catch (e) {
-            console.error([key, e])
-          }
-        }
-        return;
-      }
-      if (replacedEruda) return;
-   
-      //setTimeout(() => {
-        replaceEruda();
-      //}, 1000);
-  }
-}
-window.messageListener = window.addEventListener("message", messageListenerFunct);
+}).call(realWindow, realWindow, window)
