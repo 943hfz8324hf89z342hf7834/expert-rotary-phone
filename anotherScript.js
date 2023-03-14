52188dcb5453d8c93ece094277b0efe962e96be0
943hfz8324hf89z342hf7834 <108883510+943hfz8324hf89z342hf7834@users.noreply.github.com>
2023-03-14 21:10:43 +0100

    Update anotherScript.js

--- anotherScript.js
+++ anotherScript.js
@@ -1,19 +1,34 @@
 // ==UserScript==
-// @name         resize giant play button
-// @description  resize the giant play button that every video has
-// @version      2.0
+// @name         piped custom style
+// @description  resize the giant play button that every video has (+ some other style upgrades)
+// @version      2.1
 // @match        https://piped.kavin.rocks/*
 // @match        https://piped.video/*
 // @run-at       document-end
 // ==/UserScript==
 
+// stylesheet of the video player piped uses
 const shakaStylesheet = [...document.styleSheets]
   .find(styleSheet => styleSheet.href?.includes("WatchVideo"));
+const shakaStylesheetRules = [...shakaStylesheet.rules];
 
-const shakaPlayButtonClass = [...shakaStylesheet.rules]
-  .find(cssRule => cssRule.selectorText == ".shaka-play-button");
+// get a css rule using its selector
+function getRuleFromSelector (selectorText) {
+  return shakaStylesheetRules
+    .find(cssRule => cssRule.selectorText == selectorText);
+}
 
-shakaPlayButtonClass.style.padding = "2%";
+// play button should be smaller
+const shakaPlayButton = getRuleFromSelector(".shaka-play-button");
+shakaPlayButton.style.padding = "2%";
 
-// funnily enough you could do all of this in a single line:
+// spinner around play button should also be smaller
+const shakaSpinner = getRuleFromSelector(".shaka-spinner");
+shakaSpinner.style.padding = "2.2%";
+
+// while being shown, play button should have a lower opacity
+const shakaPlayButtonShown = getRuleFromSelector('.shaka-controls-container[casting="true"] .shaka-play-button, .shaka-controls-container[shown="true"] .shaka-play-button');
+shakaPlayButtonShown.style.opacity = "0.5";
+
+// funnily enough you could resize the playbutton using a single line:
 // [...[...document.styleSheets].find(styleSheet => styleSheet.href?.includes("WatchVideo"))?.rules].find(cssRule => cssRule.selectorText == ".shaka-play-button").style.padding = "2%";
