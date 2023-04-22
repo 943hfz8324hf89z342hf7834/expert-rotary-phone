cdcdda6d843de884fc3907450ba66f5c5ac88de7
943hfz8324hf89z342hf7834 <108883510+943hfz8324hf89z342hf7834@users.noreply.github.com>
2023-04-22 13:22:29 +0200

    Update anotherScript.js

--- anotherScript.js
+++ anotherScript.js
@@ -2,8 +2,42 @@
 // @name         piped custom style
 // @description  resize the giant play button that every video has (+ some other style upgrades)
 // @version      2.1.1
-// @match        https://piped.*/*
-// @match        https://efy.piped.*/*
+// @match        https://piped.video/*
+// @match        https://piped.kavin.rocks/*
+// @match        https://efy.piped.pages.dev/*
+// @match        https://pipedapi.kavin.rocks/*
+// @match        https://pipedapi.tokhmi.xyz/*
+// @match        https://pipedapi.moomoo.me/*
+// @match        https://pipedapi.syncpundit.io/*
+// @match        https://api-piped.mha.fi/*
+// @match        https://watchapi.whatever.social/*
+// @match        https://piped-api.garudalinux.org/*
+// @match        https://pipedapi.rivo.lol/*
+// @match        https://pipedapi.aeong.one/*
+// @match        https://pipedapi.leptons.xyz/*
+// @match        https://piped-api.lunar.icu/*
+// @match        https://ytapi.dc09.ru/*
+// @match        https://pipedapi.pfcd.me/*
+// @match        https://pipedapi-libre.kavin.rocks/*
+// @match        https://api.yt.777.tf/*
+// @match        https://pa.mint.lgbt/*
+// @match        https://pa.il.ax/*
+// @match        https://piped-api.privacy.com.de/*
+// @match        https://pipedapi.esmailelbob.xyz/*
+// @match        https://api.piped.projectsegfau.lt/*
+// @match        https://pipedapi.in.projectsegfau.lt/*
+// @match        https://pipedapi.us.projectsegfau.lt/*
+// @match        https://api.piped.privacydev.net/*
+// @match        https://pipedapi.palveluntarjoaja.eu/*
+// @match        https://p.plibre.com/*
+// @match        https://pipedapi.smnz.de/*
+// @match        https://pipedapi.adminforge.de/*
+// @match        https://pipedapi.qdi.fi/*
+// @match        https://piped-api.hostux.net/*
+// @match        https://pdapi.vern.cc/*
+// @match        https://pipedapi.chauvet.pro/*
+// @match        https://pipedapi.berryez.xyz/*
+// @match        https://pipedapi.jotoma.de/*
 // @run-at       document-end
 // ==/UserScript==
 
