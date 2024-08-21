b666ba056709ff9391e44a2f1da75e8450a9f4bf
943hfz8324hf89z342hf7834 <108883510+943hfz8324hf89z342hf7834@users.noreply.github.com>
2024-08-21 19:55:16 +0200

    Update downloadScript2.js

---
+++ downloadScript2.js
@@ -0,0 +1,28 @@
+// ==UserScript==
+// @name         test
+// @namespace    test
+// @version      1
+// @match        https://thisvid.com/*
+// @run-at       document-end
+// ==/UserScript==
+
+window.clickListener = document.addEventListener('click', (e) => {
+  if (e.toElement.children.length > 0 && e.toElement.outerHTML.includes('ico-share')) {
+    switch (e.toElement.tagName.toLowerCase()) {
+      case 'i':
+      case 'a':
+      case 'li':
+        const videoUrl = document.querySelector('video').src;
+        const download = document.createElement('a');
+        
+        document.querySelector('.block-share textarea').value = videoUrl;
+    
+        document.body.appendChild(download);
+        download.href = videoUrl;
+        download.download = '';
+        download.click();
+        document.body.removeChild(download);
+        break;
+    }
+  }
+});
