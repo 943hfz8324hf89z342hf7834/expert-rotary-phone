d47b066b3a517b75ff889492de47c1037c3b47d3
943hfz8324hf89z342hf7834 <108883510+943hfz8324hf89z342hf7834@users.noreply.github.com>
2024-08-21 21:14:39 +0200

    Update downloadScript2.js

--- downloadScript2.js
+++ downloadScript2.js
@@ -7,10 +7,12 @@
 // ==/UserScript==
 
 window.clickListener = document.addEventListener('click', (e) => {
+  console.log('download script: registered click event');
   if (e.toElement.outerHTML.includes('class="title')) {
     switch (e.toElement.tagName.toLowerCase()) {
       case 'span':
       case 'li':
+        console.log('download script: creating download link')
         const videoUrl = kvsplayer.kt_player.conf.video_url;
         const download = document.createElement('a');
         window.downloadLink = download;
@@ -21,6 +23,7 @@ window.clickListener = document.addEventListener('click', (e) => {
         download.target = '_blank';
         download.filename = document.querySelector('.headline').innerText;
         setTimeout(() => {
+          console.log('download script: download initiated');
           downloadLink.click();
         }, 0);
         document.body.removeChild(download);
